8b643e8b0b0788dfa46b386bc34123ee6f3b40ba
912-SALLE-Laurine <72121488+912-SALLE-Laurine@users.noreply.github.com>
2022-03-03 16:50:45 +0100

    mongoDB 3 queries

--- server/connexion.js
+++ server/connexion.js
@@ -4,22 +4,73 @@ const { MongoClient } = require('mongodb');
 const MONGODB_URI = "mongodb+srv://clearfashion_user:clearfashion_user@cluster0.g1yua.mongodb.net/clearfashion?retryWrites=true&w=majority";
 const MONGODB_DB_NAME = 'clearfashion';
 
-async function connect() {    try {        const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });        let connected_database = client.db(MONGODB_DB_NAME)        console.log('Connected to database ')        return connected_database    }    catch (err) {        console.error(`Error connecting to the database. \n${err}`);    }}//connect()
+async function connect() {
+    try {
+        const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        let connected_database = client.db(MONGODB_DB_NAME)
+        console.log('Connected to database ')
+        return connected_database
+    }
+    catch (err) {
+        console.error(`Error connecting to the database. \n${err}`);
+    }
+}
+//connect()
+
 
 // Insert the products
-const products = [{ id: "testproduct", name: "name" }, { id: "test2", age: 12 }];
-const dedicated_products = require('./dedicated.json');
-const montlimart_products = require('./montlimart.json');
-const adresse_paris_p1 = require('./adresseparisp1.json');
-const  adresse_paris_p2 = require('./adresseparisp2.json');
+//const dedicated_products = require('./dedicated.json');
+//const montlimart_products = require('./montlimart.json');
+//const adresse_paris_p1 = require('./adresseparisp1.json');
+//const  adresse_paris_p2 = require('./adresseparisp2.json');
+const all_products = require('./all_products.json');
 async function insert_products(products) {
     const db = await connect();
     const collection = db.collection('products');
     for (brand_products of products) {
         const result = collection.insertMany(brand_products);
     }
-    //return result
 }
-insert_products([dedicated_products, montlimart_products, adresse_paris_p1, adresse_paris_p2])
-//console.log(dedicated_products);
+//insert_products([all_products])
+
+//Find all products related to a given brands
+brand = "adresseparis"
+async function find_by_brand(brand) {
+    const db = await connect();
+    const collection = db.collection('products');
+    var query = {
+        brand: brand };
+    collection.find(query).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+}
+//find_by_brand(brand)
+
+//Find all products less than a price
+price = 10
+async function find_by_price(price) {
+    const db = await connect();
+    const collection = db.collection('products');
+    var query = { price: { $lt: price } }
+    collection.find(query).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+}
+//find_by_price(price)
+
+//Find all products sorted by price (desc)
+async function sort_by_price() {
+    const db = await connect();
+    const collection = db.collection('products');
+    var query = [{ $sort: { "price": -1 } }]
+    collection.aggregate(query).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+}
+//sort_by_price()
+
+//Find all products sorted by date
 
